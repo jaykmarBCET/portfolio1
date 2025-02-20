91a35e55051f3b359083c3876a74426777b05bf2
jaykmarBCET <jaykumar3dear@gmail.com>
2025-02-20 13:07:30 +0530

    update

--- src/connections/Backend.js
+++ src/connections/Backend.js
@@ -1,3 +1,3 @@
 
 
-export const BACKEND_URL =  "https://999k96-3000.csb.app/"             //"https://hmw83m-3000.csb.app"
+export const BACKEND_URL =  "https://999k96-3000.csb.app"             //"https://hmw83m-3000.csb.app"
