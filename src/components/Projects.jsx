ff1dd54079eb6e48ad3142c78f95b2ae42e59c96
jaykmarBCET <jaykumar3dear@gmail.com>
2025-02-24 17:01:07 +0530

    update

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { useGSAP } from "@gsap/react";
 import { cardAnimation } from "../animation/HomeAnime";
 import projects from '../data/projectData';
